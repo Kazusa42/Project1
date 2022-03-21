8c9cc57c3eeb323c8b16739e12f1edd5f87cabaa
Kitahara Kazusa <94526092+Kazusa42@users.noreply.github.com>
2022-03-21 10:16:29 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,224 @@
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from nets.yolo import YoloBody
+from nets.yolo_training import (YOLOLoss, get_lr_scheduler, set_optimizer_lr,
+                                weights_init)
+from utils.callbacks import LossHistory
+from utils.dataloader import YoloDataset, yolo_dataset_collate
+from utils.utils import get_anchors, get_classes
+from utils.utils_fit import fit_one_epoch
+
+'''
+训练自己的目标检测模型一定需要注意以下几点：
+1、训练前仔细检查自己的格式是否满足要求，该库要求数据集格式为VOC格式，需要准备好的内容有输入图片和标签
+   输入图片为.jpg图片，无需固定大小，传入训练前会自动进行resize。
+   灰度图会自动转成RGB图片进行训练，无需自己修改。
+   输入图片如果后缀非jpg，需要自己批量转成jpg后再开始训练。
+
+   标签为.xml格式，文件中会有需要检测的目标信息，标签文件和输入图片文件相对应。
+
+2、训练好的权值文件保存在logs文件夹中，每个epoch都会保存一次，如果只是训练了几个step是不会保存的，epoch和step的概念要捋清楚一下。
+   在训练过程中，该代码并没有设定只保存最低损失的，因此按默认参数训练完会有100个权值，如果空间不够可以自行删除。
+   这个并不是保存越少越好也不是保存越多越好，有人想要都保存、有人想只保存一点，为了满足大多数的需求，还是都保存可选择性高。
+
+3、损失值的大小用于判断是否收敛，比较重要的是有收敛的趋势，即验证集损失不断下降，如果验证集损失基本上不改变的话，模型基本上就收敛了。
+   损失值的具体大小并没有什么意义，大和小只在于损失的计算方式，并不是接近于0才好。如果想要让损失好看点，可以直接到对应的损失函数里面除上10000。
+   训练过程中的损失值会保存在logs文件夹下的loss_%Y_%m_%d_%H_%M_%S文件夹中
+
+4、调参是一门蛮重要的学问，没有什么参数是一定好的，现有的参数是我测试过可以正常训练的参数，因此我会建议用现有的参数。
+   但是参数本身并不是绝对的，比如随着batch的增大学习率也可以增大，效果也会好一些；过深的网络不要用太大的学习率等等。
+   这些都是经验上，只能靠各位同学多查询资料和自己试试了。
+'''  
+if __name__ == "__main__":
+    Cuda = True
+    classes_path = 'model_data/voc_classes.txt'
+    anchors_path = 'model_data/yolo_anchors.txt'
+    anchors_mask = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
+    model_path = 'model_data/yolov4_mobilenet_v1_voc.pth'
+    input_shape = [416, 416]
+
+    """
+    resnet50
+    mobilenet: v1, v2, v3
+    densenet: 121, 169, 201
+    """
+    backbone = "mobilenetv1"
+
+    pretrained = False
+
+    mosaic = True
+    label_smoothing = 0
+
+    #   在此提供若干参数设置建议，各位训练者根据自己的需求进行灵活调整：
+    #   （一）从整个模型的预训练权重开始训练： 
+    #       Init_Epoch = 0，Freeze_Epoch = 50，UnFreeze_Epoch = 100，Freeze_Train = True（默认参数）
+    #       Init_Epoch = 0，UnFreeze_Epoch = 100，Freeze_Train = False（不冻结训练）
+    #       其中：UnFreeze_Epoch可以在100-300之间调整。optimizer_type = 'sgd'，Init_lr = 1e-2。
+    #   （二）从主干网络的预训练权重开始训练：
+    #       Init_Epoch = 0，Freeze_Epoch = 50，UnFreeze_Epoch = 300，Freeze_Train = True（冻结训练）
+    #       Init_Epoch = 0，UnFreeze_Epoch = 300，Freeze_Train = False（不冻结训练）
+    #       其中：由于从主干网络的预训练权重开始训练，主干的权值不一定适合目标检测，需要更多的训练跳出局部最优解。
+    #             UnFreeze_Epoch可以在200-300之间调整，YOLOV5和YOLOX均推荐使用300。optimizer_type = 'sgd'，Init_lr = 1e-2。
+    #   （三）从0开始训练：
+    #       Init_Epoch = 0，UnFreeze_Epoch >= 300，Unfreeze_batch_size >= 16，Freeze_Train = False（不冻结训练）
+    #       其中：UnFreeze_Epoch尽量不小于300。optimizer_type = 'sgd'，Init_lr = 1e-2，mosaic = True。
+    #   （四）batch_size的设置：
+    #       在显卡能够接受的范围内，以大为好。显存不足与数据集大小无关，提示显存不足（OOM或者CUDA out of memory）请调小batch_size。
+    #       受到BatchNorm层影响，batch_size最小为2，不能为1。
+    #       正常情况下Freeze_batch_size建议为Unfreeze_batch_size的1-2倍。不建议设置的差距过大，因为关系到学习率的自动调整。
+
+    Init_Epoch = 0
+    Freeze_Epoch = 50
+    Freeze_batch_size = 16
+    #   解冻阶段训练参数
+    #   此时模型的主干不被冻结了，特征提取网络会发生改变
+    #   占用的显存较大，网络所有的参数都会发生改变
+    #   UnFreeze_Epoch          模型总共训练的epoch
+    #   Unfreeze_batch_size     模型在解冻后的batch_size
+    UnFreeze_Epoch = 100
+    Unfreeze_batch_size = 8
+    #   Freeze_Train    是否进行冻结训练
+    #                   默认先冻结主干训练后解冻训练。
+    Freeze_Train = True
+
+    #   Init_lr         模型的最大学习率
+    #   Min_lr          模型的最小学习率，默认为最大学习率的0.01
+    Init_lr = 1e-2
+    Min_lr = Init_lr * 0.01
+    #   optimizer_type  使用到的优化器种类，可选的有adam、sgd
+    #                   当使用Adam优化器时建议设置  Init_lr=1e-3
+    #                   当使用SGD优化器时建议设置   Init_lr=1e-2
+    #   momentum        优化器内部使用到的momentum参数
+    #   weight_decay    权值衰减，可防止过拟合
+    optimizer_type = "sgd"
+    momentum = 0.937
+    weight_decay = 5e-4
+
+    #   lr_decay_type   使用到的学习率下降方式，可选的有step、cos
+    lr_decay_type = "cos"
+    #   save_period     多少个epoch保存一次权值，默认每个世代都保存
+    save_period = 1
+    #   num_workers     用于设置是否使用多线程读取数据
+    #                   开启后会加快数据读取速度，但是会占用更多内存
+    #                   内存较小的电脑可以设置为2或者0
+    num_workers = 4
+
+    #   train_annotation_path   训练图片路径和标签
+    #   val_annotation_path     验证图片路径和标签
+
+    train_annotation_path = '2007_train.txt'
+    val_annotation_path = '2007_val.txt'
+
+    class_names, num_classes = get_classes(classes_path)
+    anchors, num_anchors = get_anchors(anchors_path)
+
+    model = YoloBody(anchors_mask, num_classes, backbone=backbone, pretrained=pretrained)
+    if not pretrained:
+        weights_init(model)
+    if model_path != '':
+        print('Load weights {}.'.format(model_path))
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model_dict = model.state_dict()
+        pretrained_dict = torch.load(model_path, map_location=device)
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) == np.shape(v)}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
+
+    yolo_loss = YOLOLoss(anchors, num_classes, input_shape, Cuda, anchors_mask, label_smoothing)
+    loss_history = LossHistory("logs/", model, input_shape=input_shape)
+    
+    model_train = model.train()
+    if Cuda:
+        model_train = torch.nn.DataParallel(model)
+        cudnn.benchmark = True
+        model_train = model_train.cuda()
+
+    with open(train_annotation_path, encoding='utf-8') as f:
+        train_lines = f.readlines()
+    with open(val_annotation_path, encoding='utf-8') as f:
+        val_lines = f.readlines()
+    num_train = len(train_lines)
+    num_val = len(val_lines)
+
+    #   主干特征提取网络特征通用，冻结训练可以加快训练速度
+    #   也可以在训练初期防止权值被破坏。
+    if True:
+        UnFreeze_flag = False
+        if Freeze_Train:
+            for param in model.backbone.parameters():
+                param.requires_grad = False
+
+        batch_size = Freeze_batch_size if Freeze_Train else Unfreeze_batch_size
+        nbs = 64
+        Init_lr_fit = max(batch_size / nbs * Init_lr, 1e-4)
+        Min_lr_fit = max(batch_size / nbs * Min_lr, 1e-6)
+
+        pg0, pg1, pg2 = [], [], []  
+        for k, v in model.named_modules():
+            if hasattr(v, "bias") and isinstance(v.bias, nn.Parameter):
+                pg2.append(v.bias)    
+            if isinstance(v, nn.BatchNorm2d) or "bn" in k:
+                pg0.append(v.weight)    
+            elif hasattr(v, "weight") and isinstance(v.weight, nn.Parameter):
+                pg1.append(v.weight)   
+        optimizer = {
+            'adam': optim.Adam(pg0, Init_lr_fit, betas=(momentum, 0.999)),
+            'sgd': optim.SGD(pg0, Init_lr_fit, momentum=momentum, nesterov=True)
+        }[optimizer_type]
+        optimizer.add_param_group({"params": pg1, "weight_decay": weight_decay})
+        optimizer.add_param_group({"params": pg2})
+
+        lr_scheduler_func = get_lr_scheduler(lr_decay_type, Init_lr_fit, Min_lr_fit, UnFreeze_Epoch)
+
+        epoch_step = num_train // batch_size
+        epoch_step_val = num_val // batch_size
+        
+        if epoch_step == 0 or epoch_step_val == 0:
+            raise ValueError("数据集过小，无法继续进行训练，请扩充数据集。")
+
+        train_dataset = YoloDataset(train_lines, input_shape, num_classes, epoch_length=UnFreeze_Epoch,
+                                    mosaic=mosaic, train=True)
+        val_dataset = YoloDataset(val_lines, input_shape, num_classes, epoch_length=UnFreeze_Epoch,
+                                  mosaic=False, train=False)
+        gen = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers,
+                         pin_memory=True, drop_last=True, collate_fn=yolo_dataset_collate)
+        gen_val = DataLoader(val_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers,
+                             pin_memory=True, drop_last=True, collate_fn=yolo_dataset_collate)
+
+        for epoch in range(Init_Epoch, UnFreeze_Epoch):
+            if epoch >= Freeze_Epoch and not UnFreeze_flag and Freeze_Train:
+                batch_size = Unfreeze_batch_size
+
+                nbs = 64
+                Init_lr_fit = max(batch_size / nbs * Init_lr, 1e-4)
+                Min_lr_fit = max(batch_size / nbs * Min_lr, 1e-6)
+                lr_scheduler_func = get_lr_scheduler(lr_decay_type, Init_lr_fit, Min_lr_fit, UnFreeze_Epoch)
+
+                for param in model.backbone.parameters():
+                    param.requires_grad = True
+
+                epoch_step = num_train // batch_size
+                epoch_step_val = num_val // batch_size
+
+                if epoch_step == 0 or epoch_step_val == 0:
+                    raise ValueError("数据集过小，无法继续进行训练，请扩充数据集。")
+
+                gen = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers,
+                                 pin_memory=True, drop_last=True, collate_fn=yolo_dataset_collate)
+                gen_val = DataLoader(val_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers,
+                                     pin_memory=True, drop_last=True, collate_fn=yolo_dataset_collate)
+
+                UnFreeze_flag = True
+
+            gen.dataset.epoch_now = epoch
+            gen_val.dataset.epoch_now = epoch
+
+            set_optimizer_lr(optimizer, lr_scheduler_func, epoch)
+
+            fit_one_epoch(model_train, model, yolo_loss, loss_history, optimizer, epoch, epoch_step,
+                          epoch_step_val, gen, gen_val, UnFreeze_Epoch, Cuda, save_period)
