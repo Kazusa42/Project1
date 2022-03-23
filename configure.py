861bbb35b9ba8285bca9c89496913853048e334d
Kitahara Kazusa <94526092+Kazusa42@users.noreply.github.com>
2022-03-23 19:00:08 +0800

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,18 +1,17 @@
-
 """
 Global variable configure
 Aiming to simplify the trainging, predicting and evaluating process between different dataset
 """
 
 # frcnn basic configure
-CLASSES_PATH = r'model_data/visdrone_classes.txt'
-MODEL_PATH = r'model_data/visdrone_init.pth'
+CLASSES_PATH = r'model_data/DOTA_classes.txt'
+MODEL_PATH = r'model_data/ep266-loss0.226-val_loss0.296.pth'
 # ANCHOR_SIZE = [4, 16, 32]  # [4, 16, 32] for tiny object, [8, 16, 32] for normal type
-ANCHOR_PATH = r''
+ANCHOR_PATH = r'model_data/yolo_anchors.txt'
 IF_CUDA = r'True'
 FONT_TYPE = r'model_data/monoMMM_5.ttf'
 #  get_map
 pass
 
 #  Dataset
-DATASET_PATH = r'VisDrone'
+DATASET_PATH = r'DOTA'
