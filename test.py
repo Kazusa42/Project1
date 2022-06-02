c792077677e7f176d67e351795f918b658f268f1
Kitahara Kazusa <94526092+Kazusa42@users.noreply.github.com>
2022-06-03 00:13:21 +0900

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-c_annotation_files_dir = r'C:\Users\lyin0\Desktop\DOTA\Color_Annotations_XML\\'
-
-for file_name in os.listdir(c_annotation_files_dir):
-    new_name = r'c_' + file_name
-    os.rename(c_annotation_files_dir + file_name, c_annotation_files_dir + new_name)
